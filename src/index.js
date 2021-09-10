07932ac977575a80d5f9f4a687a4f6ed50fcd022
Charlie Weber <charlestweber@gmail.com>
2021-09-10 10:53:05 -0700

    sucessfully call api and convert dollar amount

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 // import anime from 'animejs/lib/anime.es.js'
+import CurrencyService from './service';
+
+function displayResults(response, countryCode, amount) {
+  if (response.result === "success") {
+    const conversionRate = response.conversion_rates[countryCode];
+    const convertedResult = amount * conversionRate;
+    $("#results").html(`${amount} USD => ${countryCode}: ${convertedResult}`)
+    $("#conversionRate").html(`current conversion rate: ${conversionRate}`)
+  }
+  else {
+    $("#errorDisplay").html(`there was an error: ${response.error}`)
+  }
+}
+
+
+$(document).ready(function () {
+
+  $('#exBtn').click(function () {
+    console.log('button clicked');
+    const countryCode = $('#countryId :selected').val();
+    const amount = $("#usNum").val();
+    $("#usNum").val("");
+    CurrencyService.getMoney()
+      .then(function (response) {
+        displayResults(response, countryCode, amount);
+      })
+  });
+});
--- src/service.js
+++ src/service.js
@@ -1,14 +1,15 @@
 export default class CurrencyService {
-  static getMoney(){
+  static getMoney() {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
-    .then(function(response){
-      if (response.result !== "success"){
-        throw Error(result.errortype)
-      }
-      return response.json();
-    })
-    .catch(function(error){
-      return error;
-    })
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      })
   }
 }
+  
