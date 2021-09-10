fb7f51f8f58fddaeb7fe4d2c7caa4fa2d2f9ff50
Charlie Weber <charlestweber@gmail.com>
2021-09-10 11:13:02 -0700

    add dropdown to start country; two way conversion

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import './css/styles.css';
 // import anime from 'animejs/lib/anime.es.js'
 import CurrencyService from './service';
 
-function displayResults(response, countryCode, amount) {
+function displayResults(response, startCountry) {
   if (response.result === "success") {
+    const countryCode = $('#countryId :selected').val();
+    const amount = $("#moneyNum").val();
     const conversionRate = response.conversion_rates[countryCode];
-    const convertedResult = amount * conversionRate;
-    $("#results").html(`${amount} USD => ${countryCode}: ${convertedResult}`)
+    const convertedResult = Math.round(100*(amount * conversionRate))/100;
+    $("#results").html(`${amount} ${startCountry} => ${countryCode}: ${convertedResult}`)
     $("#conversionRate").html(`current conversion rate: ${conversionRate}`)
   }
   else {
@@ -22,12 +24,11 @@ $(document).ready(function () {
 
   $('#exBtn').click(function () {
     console.log('button clicked');
-    const countryCode = $('#countryId :selected').val();
-    const amount = $("#usNum").val();
+    const startCountry = $('#startCountry :selected').val();
     $("#usNum").val("");
-    CurrencyService.getMoney()
+    CurrencyService.getMoney(startCountry)
       .then(function (response) {
-        displayResults(response, countryCode, amount);
+        displayResults(response, startCountry);
       })
   });
 });
--- src/service.js
+++ src/service.js
@@ -1,6 +1,6 @@
 export default class CurrencyService {
-  static getMoney() {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+  static getMoney(country) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${country}`)
       .then(function(response) {
         if (!response.ok) {
           throw Error(response.statusText);
